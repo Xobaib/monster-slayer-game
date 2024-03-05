be6c95c5e104474d8f7ba9164d243f246205c860
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-03-05 20:16:10 +0330

    Project Setup & First Methods

---
+++ app.js
@@ -0,0 +1,26 @@
+function getRndomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+
+  methods: {
+    attackMonster() {
+      attackValue = getRndomValue(5, 12);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      attackValue = getRndomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
